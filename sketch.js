01efbc58789cf751ee6822c86eaee53192de5742
KunalPawar0207 <72133375+KunalPawar0207@users.noreply.github.com>
2020-10-27 13:11:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,6 +31,6 @@ bin.display();
 
 function keyPressed(){
 	if(keyCode == UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:40,y:-40})
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:65,y:-65})
 	}
 }
